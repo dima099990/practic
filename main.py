9e63f4fc1acbadb6dd4430c70403468c07d0d6e2
dima099990 <77355167+dima099990@users.noreply.github.com>
2023-06-15 00:15:07 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+"""Основной файл приложения."""
+
+from logic import KeyboardTrainer
+
+
+def main():
+    """Запуск приложения"""
+    KeyboardTrainer()
+
+
+if __name__ == "__main__":
+    main()
